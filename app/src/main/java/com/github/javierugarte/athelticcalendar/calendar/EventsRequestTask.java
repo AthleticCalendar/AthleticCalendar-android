4e47c954353b1f1744ef34d182f01f31f9493ae0
Javier González <javierugarte91@gmail.com>
2016-08-23 20:56:07 +0200

    Added request get events and insert events

---
+++ app/src/main/java/com/github/javierugarte/athelticcalendar/calendar/EventsRequestTask.java
@@ -0,0 +1,91 @@
+package com.github.javierugarte.athelticcalendar.calendar;
+
+import android.os.AsyncTask;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GooglePlayServicesAvailabilityIOException;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.model.Events;
+
+import java.io.IOException;
+
+/**
+ * Copyright 2016 Javier González
+ * All right reserved.
+ */
+
+public class EventsRequestTask extends AsyncTask<Void, Void, Events> {
+    private com.google.api.services.calendar.Calendar mService = null;
+    private String calendarId = null;
+    private Exception mLastError = null;
+    private GoogleCalendar.OnEventsResponseListener l = null;
+
+    public EventsRequestTask(GoogleAccountCredential credential, String calendarId, GoogleCalendar.OnEventsResponseListener l) {
+        HttpTransport transport = AndroidHttp.newCompatibleTransport();
+        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+        mService = new com.google.api.services.calendar.Calendar.Builder(
+                transport, jsonFactory, credential)
+                .setApplicationName("Google Calendar API Android Quickstart")
+                .build();
+
+        this.calendarId = calendarId;
+        this.l = l;
+    }
+
+    /**
+     * Background task to call Google Calendar API.
+     * @param params no parameters needed for this task.
+     */
+    @Override
+    protected Events doInBackground(Void... params) {
+        try {
+            return getDataFromApi();
+        } catch (Exception e) {
+            mLastError = e;
+            cancel(true);
+            return null;
+        }
+    }
+
+    /**
+     * Fetch a list of the next 10 events from the primary calendar.
+     * @return List of Strings describing returned events.
+     * @throws IOException
+     */
+    private Events getDataFromApi() throws IOException {
+        DateTime now = new DateTime(System.currentTimeMillis());
+        return mService.events().list(calendarId)
+                .setTimeMin(now)
+                .setOrderBy("startTime")
+                .setSingleEvents(true)
+                .execute();
+    }
+
+    @Override
+    protected void onPostExecute(Events events) {
+        if (l != null) {
+            l.onEvents(events);
+        }
+    }
+
+    @Override
+    protected void onCancelled() {
+        if (mLastError != null) {
+            if (mLastError instanceof GooglePlayServicesAvailabilityIOException) {
+                l.onError((GooglePlayServicesAvailabilityIOException) mLastError);
+            } else if (mLastError instanceof UserRecoverableAuthIOException) {
+                l.onErrorAuth((UserRecoverableAuthIOException) mLastError);
+            } else {
+                l.onError(mLastError);
+            }
+        } else {
+            l.onCancel();
+        }
+    }
+}
+
---
+++ app/src/main/java/com/github/javierugarte/athelticcalendar/calendar/GoogleCalendar.java
@@ -0,0 +1,46 @@
+package com.github.javierugarte.athelticcalendar.calendar;
+
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GooglePlayServicesAvailabilityIOException;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.Events;
+
+/**
+ * Copyright 2016 Javier González
+ * All right reserved.
+ */
+public class GoogleCalendar {
+
+    public interface OnEventsResponseListener {
+        void onEvents(Events events);
+        void onCancel();
+        void onError(GooglePlayServicesAvailabilityIOException error);
+        void onErrorAuth(UserRecoverableAuthIOException error);
+        void onError(Exception error);
+    }
+
+    /**
+     *
+     * @param calendarId by default is "primary"
+     */
+    public void getEvents(String calendarId, GoogleAccountCredential credential, OnEventsResponseListener listener) {
+        new EventsRequestTask(credential, calendarId, listener).execute();
+    }
+
+    public interface OnInsertEventResponseListener {
+        void onEventCreated(Event event);
+        void onCancel();
+        void onError(GooglePlayServicesAvailabilityIOException error);
+        void onErrorAuth(UserRecoverableAuthIOException error);
+        void onError(Exception error);
+    }
+
+    public void insertEvent(Event event, String calendarId, GoogleAccountCredential credential, OnInsertEventResponseListener listener) {
+        new InsertEventRequestTask(event, calendarId, credential, listener).execute();
+    }
+
+
+
+
+}
