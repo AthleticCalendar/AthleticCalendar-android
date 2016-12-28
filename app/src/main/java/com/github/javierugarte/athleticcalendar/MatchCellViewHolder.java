c4fe58c3b9b9e9420be44248b23605e90ecd8394
Javier González <javierugarte91@gmail.com>
2016-12-28 21:05:59 +0100

    New cell design

--- app/src/main/java/com/github/javierugarte/athleticcalendar/MatchCellViewHolder.java
+++ app/src/main/java/com/github/javierugarte/athleticcalendar/MatchCellViewHolder.java
@@ -16,25 +16,37 @@ import com.bumptech.glide.Glide;
  */
 public class MatchCellViewHolder extends RecyclerView.ViewHolder {
 
+    enum State {
+        ADDED,
+        ADD,
+        MODIFY
+    }
+
     private final Context context;
     private final View view;
 
-    private final TextView titleTextView;
+    private State state = State.ADD;
+
+    private final TextView team1TextView;
+    private final TextView team2TextView;
     private final TextView dateTextView;
+    private final TextView hourTextView;
     private final ImageView team1ImageView;
     private final ImageView team2ImageView;
 
-    private final ImageView actionImageView;
+    private final TextView stateTextView;
 
     public MatchCellViewHolder(View v, Context context) {
         super(v);
         this.context = context;
         this.view = v;
-        titleTextView = (TextView) v.findViewById(R.id.tv_title);
+        team1TextView = (TextView) v.findViewById(R.id.tv_team1);
+        team2TextView = (TextView) v.findViewById(R.id.tv_team2);
         dateTextView = (TextView) v.findViewById(R.id.tv_date);
+        hourTextView = (TextView) v.findViewById(R.id.tv_hour);
         team1ImageView = (ImageView) v.findViewById(R.id.iv_team1);
         team2ImageView = (ImageView) v.findViewById(R.id.iv_team2);
-        actionImageView = (ImageView) v.findViewById(R.id.iv_action_icon);
+        stateTextView = (TextView) v.findViewById(R.id.tv_state);
         v.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -46,23 +58,54 @@ public class MatchCellViewHolder extends RecyclerView.ViewHolder {
     public void bind(Match match){
 
         CharSequence date = null;
+        CharSequence hour = null;
         if (match.getStartTime() != null) {
-            date = DateFormat.format("dd-MM-yyyy kk:mm", match.getStartTime());
+            date = DateFormat.format("dd/MM/yyyy", match.getStartTime());
+            hour = DateFormat.format("kk:mm", match.getStartTime());
         }
 
-        titleTextView.setText(match.getTitle());
+        team1TextView.setText(match.getTeam1());
+        team2TextView.setText(match.getTeam2());
         dateTextView.setText(date);
+        hourTextView.setText(hour);
         Glide.with(context).load(match.getTeam1Shield()).into(team1ImageView);
         Glide.with(context).load(match.getTeam2Shield()).into(team2ImageView);
 
         if (match.isDifferent()) {
-            actionImageView.setImageResource(R.drawable.ic_modify_black);
+            state = State.MODIFY;
         } else {
-            actionImageView.setImageResource(R.drawable.ic_done_black);
+            state = State.ADDED;
+        }
+        if (!match.exists()) {
+            state = State.ADD;
         }
 
-        if (!match.exists()) {
-            actionImageView.setImageResource(R.drawable.ic_add_black);
+        setState(state);
+
+    }
+
+    private void setState(State state) {
+        switch (state) {
+            case ADDED:
+                stateTextView.setText("ADDED");
+                stateTextView.setTextColor(getColor(R.color.added));
+                break;
+            case ADD:
+                stateTextView.setText("ADD");
+                stateTextView.setTextColor(getColor(R.color.add));
+                break;
+            case MODIFY:
+                stateTextView.setText("MODIFY");
+                stateTextView.setTextColor(getColor(R.color.modify));
+                break;
+        }
+    }
+
+    private int getColor(int color) {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            return context.getResources().getColor(color, context.getTheme());
+        } else {
+            return context.getResources().getColor(color);
         }
     }
 
